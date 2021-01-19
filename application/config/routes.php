e8ac0a5a50e41bd64204ebf41b93c67eeda9f211
Yan Yanz <chaiyan,kuan@gmail.com>
2021-01-19 22:58:29 +0700

    edit index v4

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'products_control';
+$route['default_controller'] = 'Products_control';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['(:any)'] = 'views/$1';
