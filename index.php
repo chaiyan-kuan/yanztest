e3f183c2c0717b4da4f4fa2429ad328a245e91dd
chaiyan-kuan <77658208+chaiyan-kuan@users.noreply.github.com>
2021-01-19 20:39:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>จัดรายการอุปกรณ์คอมพิวเตอร์</title>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/custom.css">
+
 
 
 </head>
 <body>
-
+<h1>hello world
 
 
 </body>
