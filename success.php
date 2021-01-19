2af5011102784f54a43eff515145f8dbedf9de6f
chaiyan-kuan <77658208+chaiyan-kuan@users.noreply.github.com>
2021-01-19 12:57:58 +0700

    Add files via upload

---
+++ success.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets  /css/bootstrap.css">
+</head>
+<body>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active" aria-current="success">Success</li>
+      <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>">Register</a></li>
+      <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>index.php/ShowAll">ShowAll</a></li>
+      <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>index.php/request/login">Login</a></li>
+    </ol>
+  </nav>
+<a href ="<?php echo base_url(); ?>index.php/request/login" class="badge badge-success">This Success</a>
+</body>
+</html>
